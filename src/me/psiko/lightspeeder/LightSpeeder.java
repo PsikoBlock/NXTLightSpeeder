9a0e9c6c36284c3192b36ca5cda3c6f5cefdeb02
PsikoBlock <filipp@psiko.me>
2013-03-11 22:17:43 +0100

    Added helloworld message

---
+++ src/me/psiko/lightspeeder/LightSpeeder.java
@@ -0,0 +1,13 @@
+package me.psiko.lightspeeder;
+
+import lejos.nxt.Button;
+
+public class LightSpeeder {
+
+	public static void main(String[] args) {
+		System.out.println("Hai User!!!1");
+		Button.waitForAnyPress();
+
+	}
+
+}
